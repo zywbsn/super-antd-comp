97ae5a73e067e14d69c75fd5d82f5d322f3a9ea1
silencelamb <ooooooooooos@163.com>
2023-10-02 20:13:18 +0800

    SuperForm

--- packages/SuperForm/index.jsx
+++ packages/SuperForm/index.jsx
@@ -9,26 +9,28 @@ const Item = (props) => {
     return (
       <Form.Item
         style={{
-          width: search ? "25%" : "100%",
+          width: search ? "25%" : "50%",
         }}
         key={item.name}
         label={item.label}
         name={item.name}
         rules={item.rules}
-        initialValue={item.type === "radio" ? item.list[0].value : ""}
+        initialValue={item.type === "radio" ? item.list[0].value : item.mode === "multiple" ? [] : ""}
       >
         {item.type === "radio" ?
           <Radio.Group options={item.list} />
           : item.type === "select" ?
             <Select
+              mode={item.mode}
+              // defaultValue={[]}
               placeholder={item.placeholder}
               options={item.list}
             />
             : item.type === "date" ?
               <DatePicker format="YYYY-MM-DD" className="w-full" />
               :
-              <Input placeholder={item.placeholder} />}
-
+              <Input placeholder={item.placeholder} />
+        }
       </Form.Item>
     )
   });
@@ -37,8 +39,31 @@ const Item = (props) => {
 
 const SuperForm = (props) => {
   const [form] = Form.useForm();
-  const { formItems, formConfig, search } = props;
+  const { formItems, formConfig, search, double, submitMethod, leftBtn, rightBtn } = props;
 
+
+  //按钮列表
+  const btnList = [
+    ...leftBtn || [],
+    {
+      text: "重置",
+      buttonConfig: {
+        type: "default"
+      },
+      methods: { onClick: () => onReset() }
+    },
+    {
+      className: "mr-0",
+      text: "提交",
+      buttonConfig: {
+        type: "primary"
+      },
+      methods: { onClick: () => onSubmit() }
+    },
+    ...rightBtn || []
+  ];
+
+  //提交表单
   const onSubmit = async () => {
     const values = await form.validateFields();
     formItems.map((item) => {
@@ -46,7 +71,12 @@ const SuperForm = (props) => {
         values[item.name] = dayjs(values[item.name].$d).format("YYYY-MM-DD");
       }
     });
-    console.log("onSubmit", values);
+    submitMethod(values);//传给父组件
+  };
+
+  //重置表单
+  const onReset = () => {
+    form.resetFields();
   };
 
   return (
@@ -54,23 +84,29 @@ const SuperForm = (props) => {
       <Form
         form={form}
         labelCol={{
-          span: search ? 6 : 2,
+          span: search ? 6 : 4,
         }}
         wrapperCol={{
-          span: search ? 24 : 8,
+          span: search ? 24 : 24,
         }}
         {...formConfig}
-        className="flex flex-wrap"
+        // className={search ? "flex flex-wrap" : ""}
+        style={!double || {
+          display: "flex",
+          flexWrap: "wrap",
+          width: "100%"
+        }}
+
       >
         <Item items={formItems} search={search} />
       </Form>
-      <SuperButton
-        text="SuperButton"
-        buttonConfig={{
-          type: "primary"
-        }}
-        methods={{ onClick: () => onSubmit() }}
-      />
+      <div className={double ? "w-full text-right" : "w-1/2 text-right"}>
+        {btnList.map((item) => (
+          <React.Fragment key={item.text}>
+            <SuperButton {...item} />
+          </React.Fragment>
+        ))}
+      </div>
     </>
   )
 };
