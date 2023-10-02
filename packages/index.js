16a7eba0ae0c945d2068e80fc2dfa9ecd9330f5d
silencelamb <ooooooooooos@163.com>
2023-10-02 19:46:14 +0800

    组件导出

--- packages/index.js
+++ packages/index.js
@@ -1,14 +1,19 @@
 import SuperButton from "./SuperButton/index";
 import SuperTable from "./SuperTable/index";
+import SuperForm from "./SuperForm/index";
 
-export { SuperButton, SuperTable };
+export { SuperButton, SuperTable, SuperForm };
 
-const components = [SuperButton, SuperTable];
+// const components = [SuperButton, SuperTable, SuperForm];
 
-const install = (App, options) => {
-  components.forEach((component) => {
-    App.component(component.name, component);
-  });
-};
+// const install = (App, options) => {
+//   console.log(App, options, "11313");
+//   // for (const key in components) {
+//   //   App.component(components[key].name, components[key]);
+//   // }
+//   components.forEach((component) => {
+//     App.component(component.name, component);
+//   });
+// };
 
-export default { install };
+// export default { install };
