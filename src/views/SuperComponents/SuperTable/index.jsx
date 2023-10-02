8d5fd50e54455e4a444692745b8f35131d03b4b0
silencelamb <ooooooooooos@163.com>
2023-10-02 16:03:37 +0800

    测试 SuperTable

---
+++ src/views/SuperComponents/SuperTable/index.jsx
@@ -0,0 +1,188 @@
+import React from "react";
+import { Tag, Space } from "antd";
+import { SuperTable } from "../../../../packages/index";
+
+const SuperTableIndex = () => {
+  const columns = [
+    {
+      key: 'sort',
+    },
+    {
+      title: 'msg',
+      children: [{
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: (text) => <a>{text}</a>,
+      },
+      {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+      }]
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: (_, { tags }) => (
+        <>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? 'geekblue' : 'green';
+            if (tag === 'loser') {
+              color = 'volcano';
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <a>Invite {record.name}</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ];
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sydney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+    {
+      key: '4',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sydney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+    {
+      key: '5',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sydney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+    {
+      key: '6',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sydney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+    {
+      key: '7',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sydney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+    {
+      key: '8',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sydney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+    {
+      key: '9',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sydney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+    {
+      key: '10',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sydney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+    {
+      key: '11',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sydney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
+
+  return (
+    <>
+      <SuperTable
+        leftButton={{
+          custom: false,
+          buttonList: [
+            {
+              text: "SuperButton",
+              tooltipStatus: true,
+              tooltipConfig: {
+                title: "tooltipConfig", placement: "topLeft", arrow: { pointAtCenter: true }
+              },
+              buttonConfig: {
+                type: "primary"
+              },
+              methods: { onClick: () => console.log("SuperButton") }
+            },
+            {
+              text: "SuperButton2",
+              buttonConfig: {
+                type: "primary"
+              },
+              methods: { onClick: () => console.log("SuperButton2") }
+            }
+          ]
+        }}
+        tableConfig={{
+          rowKey: "key",
+          columns,
+          // expandable: {
+          //   expandedRowRender: (record) => (
+          //     <p
+          //       style={{
+          //         margin: 0,
+          //       }}
+          //     >
+          //       {record.address}
+          //     </p>
+          //   ),
+          //   rowExpandable: (record) => record.address !== 'Not Expandable',
+          // },
+          dataSource: data
+        }}
+      />
+    </>
+  )
+};
+
+export default SuperTableIndex;
---
+++ src/views/SuperComponents/router.jsx
@@ -0,0 +1,8 @@
+import { AppstoreOutlined } from "@ant-design/icons";
+
+export default {
+  name: "SuperAntd",
+  sort: 3,
+  isFather: true,
+  icon: <AppstoreOutlined />
+};
